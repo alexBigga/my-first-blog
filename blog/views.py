8962a27294d08f5dceafa731e773a113eb27d286
Schoemaker <schoemaker@wiwi.uni-wuppertal.de>
2019-06-13 10:59:15 +0200

    View und Template fuer BLogpost-Details zusammen mit CSS derstellt

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -11,3 +12,7 @@ def post_list(request):
     # blog/post_list.html: Template
     # {}: Zur Verwendung durch das Template, hier posts
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
